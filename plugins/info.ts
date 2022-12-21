02a8aa2e676fab1774a08799ae7f0772c2e67199
MUSTAFA ERÇEL <mustafa.ercel@tsoft.com.tr>
2022-12-21 21:11:43 +0300

    Fix | Tour Todos

--- plugins/info.ts
+++ plugins/info.ts
@@ -2,7 +2,8 @@
 export default defineNuxtPlugin((nuxtApp) => {
     return{
         provide:{
-            phone : '+43 1 236 60 60' 
+            phone : '+43 1 236 60 60' ,
+            url   : 'https://martireisen.at'
         }
     }
 })
