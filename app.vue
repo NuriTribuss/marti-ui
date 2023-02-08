46242177ac0e6d635723a17171f7ed3cd1d1987e
Mustafa ERÇEL <mustafa.ercel@tsoft.com.tr>
2023-02-08 20:20:07 +0300

    Fix | Seo Issues

--- app.vue
+++ app.vue
@@ -7,3 +7,15 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+const route = useRoute()
+useHead(() => ({
+  link: [
+    {
+      rel: 'canonical',
+      href: 'https://www.martireisen.at' + route.path,
+    },
+  ],
+}))
+</script>
--- components/Landing/Country.vue
+++ components/Landing/Country.vue
@@ -1,12 +1,12 @@
 <template>
-  <BreadCrumbSmall :step="[meta.data.title]" />
+  <BreadCrumbSmall :step="[content.title]" />
   
   <section  class="breadcrumb-area py-2">
     <div class="breadcrumb-wrap">
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
-            <div class="bg-rgb-6 p-4 my-4 text-white" v-show="!loader.landing">
+            <div class="bg-rgb-6 p-4 my-4 text-white" >
               <h1>{{ content.title }}</h1>
               <div>{{ content.subtitle }}</div>
             </div>
@@ -15,16 +15,15 @@
             </div>
           </div>
         </div>
-        
       </div>
     </div>
   </section>
   <section>
     <div class="container">
-      <div v-show="loader.landing">
+      <div v-show="pending">
           <LoaderLanding/>
       </div>
-      <div class="row my-4">
+      <div class="row my-4" v-if="content.title">
         <div class="col-lg-12">
             <h2 class="text-center my-2">{{ content.second_title}}</h2>
             <p class="text-center" v-html=" content.second_subtitle"></p>
@@ -38,64 +37,53 @@
   </section>
 </template>
 
-<script>
+
+<script setup> 
 
 import search from '/modules/search';
 
-export default {
-  props: ["meta", "config"],
-  components : { search },
-  data() {
-    return {
-      record: null,
-      bg_image: "",
-      content: {},
-      breadcrumb : '',
-      loader : {
-        landing : true,
-      },
-      error : false,
-    };
-  },
-  methods: {
-    getData() {
+const content = ref({});
+const hotelList = ref(null);
+const props   = defineProps(['meta', 'config']);
 
-      let vue = this;
-      $fetch("/api/landing/zone/fetch/country/",{ method : 'POST', body: this.meta.data}).then(
-        function (result) {
-          if (!result.status) {
-            return false;
-          }
-          vue.record = result.data;
-          vue.content = vue.translate(vue.record.translate, vue.meta.data.locale);
-          vue.loader.landing = false;
-         
-          let query  = search.get();
-          query['destination'] = {
-            code : vue.record.zone_code,
-            type : 'country', 
-            name : ''
-          }
+const { pending, data: res } = await useFetch('/api/landing/zone/fetch/country/', {
+    key : 'landing_country',
+    method: 'POST',
+    body: props.meta.data
+})
 
-          vue.$refs.hotelList.getResult(query,vue.content.third_title);
-        }
-      );
-    },
-    translate(data, language) {
-      for (var i = 0; i < data.length; i++) {
+const translate = (data, language) => {
+
+    for (var i = 0; i < data.length; i++) {
         if (language == data[i].language) {
-          return data[i];
+            return data[i];
         }
-      }
+    }
+    return data[0];
+}
 
-      return data[0];
-    },
+content.value = translate(res.value.data.translate, props.meta.data.locale)
 
-  },
-  
-  mounted() {
-    this.getData();
-    this.bg_image = this.config.public.BASE_URL + this.meta.data.image;
-  },
-};
+onMounted(() => {
+
+    let query  = search.get();
+    query['destination'] = {
+      code : res.value.data.zone_code,
+      type : 'country', 
+      name : ''
+    }
+    console.log(hotelList)
+    hotelList.value.getResult(query,content.value.third_title)
+})
+
+useHead({
+    title: content.value.title,
+    meta: [
+        {
+            hid: 'description',
+            name: 'description',
+            content: content.value.subtitle
+        }
+    ]
+})
 </script>
