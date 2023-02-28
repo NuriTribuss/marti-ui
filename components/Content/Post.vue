fe2879ec4f16436402d285f579dd2559c77e992c
Mustafa ERÇEL <mustafa.ercel@tsoft.com.tr>
2023-02-28 16:31:26 +0300

    Fix | Blog Post language issue

--- components/Content/Post.vue
+++ components/Content/Post.vue
@@ -1,5 +1,5 @@
 <template>
-  <BreadCrumb v-if="record" :title="record.translate[0].name" :steps="['Blog',record.translate[0].name]" />
+  <BreadCrumb v-if="record" :title="record.name" :steps="['Blog',record.name]" />
   <section class="about-area padding-bottom-90px overflow-hidden mt-3">
         <div class="container">
             <div class="row my-4">
@@ -8,7 +8,7 @@
                         <img class="w-100" src="https://martireisen.at/data/image/posts/6/6.jpg" />
                     </div>
                 </div>
-                <div class="col-lg-8 content"  v-if="record" v-html="record.translate[0].content">
+                <div class="col-lg-8 content"  v-if="record" v-html="record.content">
                 </div>
             </div>
         </div>
@@ -28,12 +28,17 @@ export default {
   methods : {
     getPost(){
        let vue = this;
-       $fetch("/api/content/post/post/"+this.id).then(function(result){
+       $fetch("/api/content/post/post/"+this.id+'?hl='+this.locale).then(function(result){
          
           if(!result.status) {
             return false;
           }
-          vue.record = result.data;
+
+          console.log(result.data);
+          vue.record = result.data.translate.filter(function(item){
+            return item.language == vue.locale;
+          })[0];
+         // vue.record = result.data;
       })
     }
   },
