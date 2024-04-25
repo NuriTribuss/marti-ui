12c9af48dc57c73017a12a29ad34ac9d5db456bc
Yilmaz Thomas <Nuri@tribus-business.at>
2024-04-25 11:52:28 +0200

    Add starting at local Fix local usage

--- components/Hotel/CardSmall.vue
+++ components/Hotel/CardSmall.vue
@@ -1,53 +1,44 @@
 <template>
   <div class="card-item card-item-list d-block shadow-lg">
     <div class="card-img">
-      <a  href="javascript:;" @click="go(hotel.giata.hotelId,hotel.name_sef)" class="d-block">
-       <img  v-bind:src="image" alt="hotel-img" />
+      <a href="javascript:;" @click="go(hotel.giata.hotelId, hotel.name_sef)" class="d-block">
+        <img v-bind:src="image" alt="hotel-img" />
       </a>
       <span class="badge d-none">Bestseller</span>
-      <div
-        class="add-to-wishlist icon-element"
-        data-toggle="tooltip"
-        data-placement="top"
-        title="Bookmark"
-        @click="likeHotel"
-      >
+      <div class="add-to-wishlist icon-element" data-toggle="tooltip" data-placement="top" title="Bookmark"
+        @click="likeHotel">
         <i v-if="liked" class="la la-heart"></i>
         <i v-else class="la la-heart-o"></i>
 
       </div>
-      <HotelRating :rate="hotel.rating.overall"/>
-      </div>
-      
+      <HotelRating :rate="hotel.rating.overall" />
+    </div>
+
     <div class="card-body p-3 p-lg-4">
       <h3 class="card-title font-size-18">
-        <a href="javascript:;"  @click="go(hotel.giata.hotelId,hotel.name_sef)">{{hotel.name}}</a>
+        <a href="javascript:;" @click="go(hotel.giata.hotelId, hotel.name_sef)">{{ hotel.name }}</a>
       </h3>
-      <p class="card-meta">{{ hotel.location.name }}, ({{ hotel.location.region.name}})</p>
+      <p class="card-meta">{{ hotel.location.name }}, ({{ hotel.location.region.name }})</p>
       <div class="card-rating pt-1">
         <span class="ratings d-flex align-items-center">
           <i class="la la-star" v-for="i in parseInt(hotel.category) || 0" :key="i"></i>
         </span>
       </div>
-      <div
-        class="card-facility d-lg-flex align-items-center justify-content-between"
-      >
+      <div class="card-facility d-lg-flex align-items-center justify-content-between">
         <div class="mt-3 mt-lg-0 w-100">
           <div class="px-4 py-1 bg-rgb-3 ">
             <p v-if="hotel.offerList" class="font-weight-bold my-2">
-              <span v-if="offer">{{ offer.travelDate.duration }} {{$t('common.days')}}, </span> 
+              <span v-if="offer">{{ offer.travelDate.duration }} {{ $t('common.days') }}, </span>
               <span v-if="offer">{{ offer.boardType.name }}, </span>
               <span v-if="offer">{{ localize_date(offer.travelDate.fromDate) }}</span>
             </p>
-            <button
-              @click="go(hotel.giata.hotelId,hotel.name_sef)"
-              class="rounded theme-btn theme-btn-blue text-white my-2 w-100 font-weight-bold"
-            >
+            <button @click="go(hotel.giata.hotelId, hotel.name_sef)"
+              class="rounded theme-btn theme-btn-blue text-white my-2 w-100 font-weight-bold">
               <template v-if="hotel.bestPricePerPerson">
-                <small>ab</small> € {{ $n(hotel.bestPricePerPerson.value) }}
+                <small>{{ $t("common.starting") }}</small> € {{ $n(hotel.bestPricePerPerson.value) }}
               </template>
               <template v-else>
-                <small>{{$t('search.to_offer')}}</small>
+                <small>{{ $t('search.to_offer') }}</small>
               </template>
               <i class="la la-angle-right"></i>
             </button>
@@ -65,7 +56,7 @@ import { mapActions } from 'vuex';
 dayjs.locale('de');
 dayjs().locale('de').format();
 export default {
-  components : {dayjs},
+  components: { dayjs },
   props: ['hotel'],
   data() {
     return {
@@ -82,20 +73,19 @@ export default {
       liked: false,
     }
   },
-  methods : {
-    go(id,sef){
-      this.$emit('searchHotel',id,sef)
+  methods: {
+    go(id, sef) {
+      this.$emit('searchHotel', id, sef)
     },
-    localize_date(dt)
-    {
+    localize_date(dt) {
       return dayjs(dt).format('ddd DD. MMM')
     },
-    likeHotel(){
+    likeHotel() {
       this.doLikeHotel(this.cookieHotelObj);
       this.likedHotels = this.favHotels;
-      if(this.likedHotels?.find((h) => h.hotelId==this.hotel.giata.hotelId) != null){
+      if (this.likedHotels?.find((h) => h.hotelId == this.hotel.giata.hotelId) != null) {
         this.liked = true;
-      }else{
+      } else {
         this.liked = false;
       }
     },
@@ -104,11 +94,11 @@ export default {
     }),
   },
   computed: {
-    offer(){
+    offer() {
       return this.hotel.offerList ? this.hotel.offerList[0] : null;
     },
-    image(){
-      return this.hotel.mediaData.pictureUrl?.replace('150','300');
+    image() {
+      return this.hotel.mediaData.pictureUrl?.replace('150', '300');
     },
     favHotels: {
       get() {
@@ -116,17 +106,17 @@ export default {
       }
     },
   },
-  watch:{
-    favHotels (newValue, oldValue) {
+  watch: {
+    favHotels(newValue, oldValue) {
       this.likedHotels = this.favHotels;
-      if(this.likedHotels?.find((h) => h.hotelId==this.hotel.giata.hotelId) != null){
+      if (this.likedHotels?.find((h) => h.hotelId == this.hotel.giata.hotelId) != null) {
         this.liked = true;
-      }else{
+      } else {
         this.liked = false;
       }
     }
   },
-  mounted(){
+  mounted() {
     this.cookieHotelObj.name = this.hotel.name;
     this.cookieHotelObj.category = this.hotel.category;
     this.cookieHotelObj.hotelId = this.hotel.giata.hotelId;
@@ -137,9 +127,9 @@ export default {
 
     this.$store.dispatch("setLikedHotels");
     this.likedHotels = this.favHotels;
-    if(this.likedHotels?.find((h) => h.hotelId==this.hotel.giata.hotelId) != null){
+    if (this.likedHotels?.find((h) => h.hotelId == this.hotel.giata.hotelId) != null) {
       this.liked = true;
-    }else{
+    } else {
       this.liked = false;
     }
   }
