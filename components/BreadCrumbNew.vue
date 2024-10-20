44257b1af069f85e1ba62c1fcc301e0f5d4370b6
Yilmaz Thomas <Nuri@tribus-business.at>
2024-10-21 00:38:57 +0200

    Fix Breadcrumb Translation error and fix age Group in tours display

--- components/BreadCrumbNew.vue
+++ components/BreadCrumbNew.vue
@@ -8,7 +8,7 @@
                         <li class="breadcrumb-item">
                             <a class="breadcrumb-a-tag" href="/">{{ $t("header.search") }}</a>
                         </li>
-                        <li class="breadcrumb-item" v-for="(item, index) in  step " :key="index">
+                        <li class="breadcrumb-item" v-for="(item, index) in step " :key="index">
                             <svg xmlns="http://www.w3.org/2000/svg" height="14px" viewBox="0 0 24 24" width="18px"
                                 fill="#050505">
                                 <path d="M0 0h24v24H0V0z" fill="none" />
@@ -16,8 +16,11 @@
                             </svg>
                             <span v-if="index + 1 == step.length"
                                 class="la la-map-marker form-icon text-color-12 z-index-1"></span>
+                            <!-- <span v-if="index + 1 == step.length"
+                                v-html='$t("common." + item?.name) || item.name || ""'></span> -->
                             <span v-if="index + 1 == step.length"
-                                v-html='$t("common." + item.name) || item || ""'></span>
+                                v-html='$t("common." + item.name) !== "common." + item.name ? $t("common." + item.name) : item.name'></span>
+
                             <a class=" breadcrumb-a-tag" v-else :href="item.to" v-html="item.name || ''"></a>
                         </li>
                     </ol>
